a84751a0a739ade3aff1c4e3be9d4fbd562d65c2
pkr187 <pankajroman187@gmail.com>
2023-01-22 23:27:51 +0530

    [pankaj] Add:FirstName

---
+++ Regex/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regexs
+{
+    public class Patterns
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool ValidetFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, REGEX_FIRSTNAME);
+        }
+    }
+}
---
+++ Regex/Program.cs
@@ -0,0 +1,24 @@
+﻿using Regexs;
+
+namespace Regexs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Name");
+            string fName = Console.ReadLine();
+
+
+            Patterns patterns = new Patterns();
+
+
+            bool val = patterns.ValidetFirstName(fName);
+            if (val)
+                Console.WriteLine("First Name is Valide");
+            else
+                Console.WriteLine("First Name is Not Valide (First name should starts with Cap and has minimum 3 characters)");
+        }
+    }
+}
+
