6f2aae5b06f740e4e0eaa9b0433246d91a43bd06
pkr187 <pankajroman187@gmail.com>
2023-01-22 23:32:49 +0530

    [pankaj] Add:LastName

--- Regex/Patterns.cs
+++ Regex/Patterns.cs
@@ -10,10 +10,18 @@ namespace Regexs
     public class Patterns
     {
         public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string REGEX_LASTNAME = "^[A-Z]{1}[a-z]{2,}$";
 
+        //UC1 FIRST NAME
         public bool ValidetFirstName(string FirstName)
         {
             return Regex.IsMatch(FirstName, REGEX_FIRSTNAME);
+        }
+
+        //UC1 LAST NAME
+        public bool ValidLastName(string LasttName)
+        {
+            return Regex.IsMatch(LasttName, REGEX_LASTNAME);
         }
     }
 }
--- Regex/Program.cs
+++ Regex/Program.cs
@@ -9,16 +9,25 @@ namespace Regexs
             Console.WriteLine("Enter the Name");
             string fName = Console.ReadLine();
 
+            Console.WriteLine("Enter the Name");
+            string lName = Console.ReadLine();
+
 
             Patterns patterns = new Patterns();
 
-
+            //UC1 FIRST NAME
             bool val = patterns.ValidetFirstName(fName);
             if (val)
                 Console.WriteLine("First Name is Valide");
             else
                 Console.WriteLine("First Name is Not Valide (First name should starts with Cap and has minimum 3 characters)");
+
+            //UC2 LAST NAME
+            bool val1 = patterns.ValidLastName(lName);
+            if (val1)
+                Console.WriteLine("Last Name is Valide");
+            else
+                Console.WriteLine("Last Name is Not Valide (Last name should starts with Cap and has minimum 3 characters)");
         }
     }
 }
-
